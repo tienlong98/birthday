744f769686819e3ffb37ba22d4f5b686ad7f7026
PHAM THANH LONG <c210117@ccg.ac.jp>
2021-06-07 01:06:01 +0900

    upde

--- effect.js
+++ effect.js
@@ -178,9 +178,9 @@ $('document').ready(function() {
         var i;
 
         function msgLoop(i) {
-            $("p:nth-child(" + i + ")").fadeOut('slow').delay(3200).promise().done(function() {
+            $("p:nth-child(" + i + ")").fadeOut('slow').delay(1200).promise().done(function() {
                 i = i + 1;
-                $("p:nth-child(" + i + ")").fadeIn('slow').delay(3500);
+                $("p:nth-child(" + i + ")").fadeIn('slow').delay(1500);
                 if (i == 50) {
                     $("p:nth-child(49)").fadeOut('slow').promise().done(function() {
                         $('.cake').fadeIn('fast');
